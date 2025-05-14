b5d8bb56a25776bc33b670f09c4b706aa14bf6a7
Alejandro Cervantes Flores <alejandro.cervantes3@gmail.com>
2025-05-14 15:11:49 -0700

    Initial commit to push all files

---
+++ app.js
@@ -0,0 +1,99 @@
+// ########################################
+// ########## SETUP
+
+// Express
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const PORT = 5330 ;
+
+// Database
+const db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars'); // Import express-handlebars engine
+app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
+app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
+
+// ########################################
+// ########## ROUTE HANDLERS
+
+// READ ROUTES
+
+//for home 
+app.get('/', async function (req, res) {
+    try {
+        res.render('home'); // Make sure you have a views/home.hbs file
+    } catch (error) {
+        console.error('Error rendering homepage:', error);
+        res.status(500).send('Failed to load homepage.');
+    }
+});
+//for players
+app.get('/players', async function (req, res) {
+    try {
+        const query = 'SELECT * FROM Players;';
+        const [players] = await db.query(query);
+        res.render('players', { players });
+    } catch (error) {
+        console.error('Error executing query for Players:', error);
+        res.status(500).send('Error loading Players page.');
+    }
+});
+
+//for teams
+app.get('/teams', async function (req, res) {
+    try {
+        const [teams] = await db.query('SELECT * FROM Teams;');
+        res.render('teams', { teams });
+    } catch (err) {
+        res.status(500).send('Error loading Teams page');
+    }
+});
+
+//for games
+app.get('/games', async function (req, res) {
+    try {
+        const [games] = await db.query('SELECT * FROM Games;');
+        res.render('games', { games });
+    } catch (err) {
+        res.status(500).send('Error loading Games page');
+    }
+});
+
+//for statistics
+app.get('/statistics', async function (req, res) {
+    try {
+        const [statistics] = await db.query('SELECT * FROM Statistics;');
+        res.render('statistics', { statistics });
+    } catch (err) {
+        res.status(500).send('Error loading Games page');
+    }
+});
+
+//for playerstatistics
+app.get('/playerstatistics', async function (req, res) {
+    try {
+        const [playerstatistics] = await db.query('SELECT * FROM PlayerStatistics;');
+        res.render('playerstatistics', { playerstatistics });
+    } catch (err) {
+        res.status(500).send('Error loading Games page');
+    }
+});
+
+
+
+
+// ########################################
+// ########## LISTENER
+
+app.listen(PORT, function () {
+    console.log(
+        'Express started on http://localhost:' +
+            PORT +
+            '; press Ctrl-C to terminate.'
+    );
+});
